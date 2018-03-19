042732761fbec6d311d2e7c12c4f4cbf48440f9a
Giyas Guliyev <g.g.giyas@gmail.com>
2018-03-19 20:40:50 +0300

    precompiled assets

---
+++ public/assets/ckeditor/config-ebea4971071989fef119b6f1694d7595024cebac6fbdb5820206b2b4e5192fca.js
@@ -0,0 +1,13 @@
+CKEDITOR.editorConfig = function (config) {
+  // ... other configuration ...
+
+  // config.toolbar_mini = [
+  //   ["Bold",  "Italic",  "Underline",  "Strike",  "-",  "Subscript",  "Superscript"],
+  // ];
+  // config.toolbar = "mini";
+
+  // ... rest of the original config.js  ...
+  config.enterMode = CKEDITOR.ENTER_BR;
+  config.autoParagraph = false;
+}
+;
